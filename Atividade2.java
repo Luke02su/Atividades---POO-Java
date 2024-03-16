682e8dd9a7fc34d42ddbd9648cd772f15de12ae6
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-03-16 10:48:50 -0300

    Add files via upload

--- Atividade2.java
+++ Atividade2.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 public class Atividade2 {
     
     static Conta c1 = new Conta(0, "null", 0); // objeto global
+    static Scanner scan = new Scanner(System.in);
     
-    public static void main(String[] args) {
-   
-        Scanner scan = new Scanner(System.in);
-        
+    public static void main(String[] args) { // método principal
+       
         int escolha=0;
         
         do {
-            System.out.println("1. Criar uma conta\n2. Imprimir dados da conta\n3. Sair do programa\n");
+            System.out.println("1. Criar uma conta\n2. Imprimir dados da conta\n3.Atualizar nome\n4. Atualizar valor\n5. Sacar valor\n6.Sair do programa\n");
             escolha = scan.nextInt();
             
             switch(escolha) {
@@ -22,15 +21,23 @@ public class Atividade2 {
                 case 2: 
                     imprimirDados();
                     break;
+                case 3:
+                    atualizarNome();
+                    break;
+                case 4: {
+                    atualizarValor();
+                    break;
+                }
+                case 5: 
+                    sacarValor();
+                    break;
             }
             
-        } while (escolha != 3);
+        } while (escolha != 6);
     }
     
     public static void criarConta() {
-        
-        Scanner scan = new Scanner(System.in);
-             
+                  
         System.out.print("Digite o  numero da conta: ");
         int numero = scan.nextInt();
         c1.setNumero(numero);
@@ -48,12 +55,43 @@ public class Atividade2 {
             double valor = scan.nextDouble();
             c1.setValor(valor);
         }
+        imprimirDados();
     }
-  
+
         public static void imprimirDados() {           
             System.out.print("Conta " + c1.getNumero());
             System.out.print(", Titular: "+ c1.getTitular());
             System.out.println(", Saldo: " + c1.getValor());
         }
-     
-   }
+        
+        public static void atualizarNome() {
+            scan.nextLine();
+            System.out.println("Altere o nome do titular: ");
+            String titular = scan.nextLine();
+            c1.setTitular(titular);
+            imprimirDados();
+        }
+        
+        public static void atualizarValor() {
+            System.out.println("Digite um valor de depósito: ");
+            double valor = c1.getValor();
+            double valorDeposito = scan.nextDouble();
+            valor += valorDeposito;
+            c1.setValor(valor);
+            imprimirDados();
+        }
+        
+        public static void sacarValor() {
+            System.out.print("Digite o valor do saque: ");
+            double saque = scan.nextDouble();
+            if(c1.getValor() < saque + 5) {
+                System.out.println("Operação não realizada. Saldo insuficiente");
+            } else {
+                double valor = c1.getValor();
+                valor -= saque;
+                c1.setValor(valor);
+            }
+            imprimirDados();
+        }
+    }
+   
