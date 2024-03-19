34ddd7750a2edbcc6b09ad0e6d90f42a72421391
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-03-18 22:44:29 -0300

    Versão final
    Versão final.

--- Atividade2.java
+++ Atividade2.java
@@ -1,13 +1,11 @@
 package atividade2;
 import java.util.Scanner;
 
-public class Atividade2 {
-    
-    static Conta c1 = new Conta(0, "null", 0.00); // objeto global
-    static Scanner scan = new Scanner(System.in);
-    static boolean status = false;
-    
+public class Atividade2 { 
     public static void main(String[] args) { // método principal
+        
+    Conta c1 = new Conta(0, "null", 0.00); // objeto global
+    Scanner scan = new Scanner(System.in);
        
         int escolha=0;
         
@@ -18,108 +16,23 @@ public class Atividade2 {
             
             switch(escolha) {
                 case 1:
-                    criarConta();
+                    c1.criarConta();
                     break;
                 case 2: 
-                    imprimirDados();
+                    c1.imprimirDados();
                     break;
                 case 3:
-                    atualizarNome();
+                    c1.atualizarNome();
                     break;
                 case 4: {
-                    atualizarValor();
+                    c1.atualizarValor();
                     break;
                 }
                 case 5: 
-                    sacarValor();
+                    c1.sacarValor();
                     break;
             }
         } while (escolha != 6);
         System.out.println();
     }
-    
-    public static void criarConta() {
-                  
-        System.out.print("\nDigite o número da conta: ");
-        int numero = scan.nextInt();
-        c1.setNumero(numero);
-        
-        scan.nextLine(); //quebra de linha para ler a string seguinte
-        
-        System.out.print("Digite o nome titular da conta: ");
-        String titular = scan.nextLine();
-        c1.setTitular(titular);
-        
-        char opcao = 's';
-        while(opcao != 's'  || opcao != 'n') {
-            System.out.print("Há um depósito inicial (s/n)? ");
-            opcao = scan.nextLine().charAt(0); //converte string em char;
-            if(opcao == 's') {
-                System.out.print("Digite o valor de depósito inicial: ");
-                double valor = scan.nextDouble();
-                c1.setValor(valor);
-                break;
-            } else if(opcao == 'n'){
-                break;
-            }
-        }
-        status = true;
-        imprimirDados();
-    }
-
-    public static void imprimirDados() {
-        System.out.println();
-        if(status == false) {
-            System.out.println("A conta não foi criada.\n");
-        } else {
-            System.out.println("Dados atualizados da conta:");
-            System.out.print("Conta " + c1.getNumero());
-            System.out.print(", Titular: "+ c1.getTitular());
-            System.out.printf(", Saldo: $ %.2f", c1.getValor());
-            System.out.println("\n");
-        }
-    }
-
-    public static void atualizarNome() {
-        if(status == false) {
-            System.out.println("\nA conta não foi criada.\n");
-        } else {
-            scan.nextLine();
-            System.out.print("\nAltere o nome do titular: ");
-            String titular = scan.nextLine();
-            c1.setTitular(titular);
-            imprimirDados();
-        }
-    }
-
-    public static void atualizarValor() {
-        if(status == false) {
-            System.out.println("\nA conta não foi criada.\n");
-        } else {
-            System.out.print("\nDigite um valor de depósito: ");
-            double valor = c1.getValor();
-            double valorDeposito = scan.nextDouble();
-            valor += valorDeposito;
-            c1.setValor(valor);
-            imprimirDados();
-        }
-    }
-
-    public static void sacarValor() {
-        if(status == false) {
-            System.out.println("\nA conta não foi criada.\n");
-        } else {
-            System.out.print("\nDigite o valor do saque: ");
-            double saque = scan.nextDouble();
-            if(c1.getValor() < (saque + 5) || saque <= 0.00) { // taxa de 5 reais ao sacar
-                System.out.print("\nOperação não realizada. Saldo insuficiente.");
-            } else {
-                double valor = c1.getValor();
-                valor -= (saque + 5);
-                c1.setValor(valor);
-            }
-        imprimirDados();
-        }
-    }
 }
-   
--- Conta.java
+++ Conta.java
@@ -1,9 +1,13 @@
 package atividade2;
 
+import java.util.Scanner;
+
 public class Conta {
     private int numero;
     private String titular;
     private double valor;
+    private boolean criado;
+    private Scanner scan;
 
     public Conta(int numero, String titular, double valor) {
         this.numero = numero;
@@ -11,28 +15,110 @@ public class Conta {
         this.valor = valor;
     }
     
-    public int getNumero() {
+        public int getNumero() {
         return this.numero;
     }
-    
+
     public void setNumero(int numero) {
         this.numero = numero;
     }
-    
+
     public String getTitular() {
         return this.titular;
     }
-    
+
     public void setTitular(String titular) {
         this.titular = titular;
     }
-    
+
     public double getValor() {
         return this.valor;
     }
-    
+
     public void setValor(double valor) {
         this.valor = valor;
     }
+
+    public void criarConta() {
+        System.out.print("\nDigite o número da conta: ");
+        numero = this.scan.nextInt();
+        this.setNumero(numero);
+
+        this.scan.nextLine(); // quebra de linha para ler a string seguinte
+
+        System.out.print("Digite o nome titular da conta: ");
+        titular = this.scan.nextLine();
+        this.setTitular(titular);
+
+        char opcao = 's';
+        while (opcao != 's' || opcao != 'n') {
+            System.out.print("Há um depósito inicial (s/n)? ");
+            opcao = this.scan.nextLine().charAt(0); // converte string em char;
+            if (opcao == 's') {
+                System.out.print("Digite o valor de depósito inicial: ");
+                valor = this.scan.nextDouble();
+                this.setValor(valor);
+                break;
+            } else if (opcao == 'n') {
+                break;
+            }
+        }
+        this.criado = true;
+        this.imprimirDados();
+    }
+
+    public void imprimirDados() {
+        System.out.println();
+        if (!this.criado) {
+            System.out.println("A conta não foi criada.\n");
+        } else {
+            System.out.println("Dados atualizados da conta:");
+            System.out.print("Conta " + this.getNumero());
+            System.out.print(", Titular: " + this.getTitular());
+            System.out.printf(", Saldo: $ %.2f", this.getValor());
+            System.out.println("\n");
+        }
+    }
+
+    public void atualizarNome() {
+        if (!this.criado) {
+            System.out.println("\nA conta não foi criada.\n");
+        } else {
+            this.scan.nextLine();
+            System.out.print("\nAltere o nome do titular: ");
+            titular = this.scan.nextLine();
+            this.setTitular(titular);
+            this.imprimirDados();
+        }
+    }
+
+    public void atualizarValor() {
+        if (!this.criado) {
+            System.out.println("\nA conta não foi criada.\n");
+        } else {
+            System.out.print("\nDigite um valor de depósito: ");
+            valor = this.getValor();
+            double valorDeposito = this.scan.nextDouble();
+            valor += valorDeposito;
+            this.setValor(valor);
+            this.imprimirDados();
+        }
+    }
+
+    public void sacarValor() {
+        if (!this.criado) {
+            System.out.println("\nA conta não foi criada.\n");
+        } else {
+            System.out.print("\nDigite o valor do saque: ");
+            double saque = this.scan.nextDouble();
+            if (this.getValor() < (saque + 5) || saque <= 0.00) { // taxa de 5 reais ao sacar
+                System.out.print("\nOperação não realizada. Saldo insuficiente.");
+            } else {
+                valor = this.getValor();
+                valor -= (saque + 5);
+                this.setValor(valor);
+            }
+            this.imprimirDados();
+        }
+    }
 }
-    
