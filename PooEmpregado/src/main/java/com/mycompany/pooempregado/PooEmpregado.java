d5ce679c0a397dc57542bed15aebe8642b930f7a
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-03-26 22:35:59 -0300

    Update PooEmpregado.java

--- PooEmpregado/src/main/java/com/mycompany/pooempregado/PooEmpregado.java
+++ PooEmpregado/src/main/java/com/mycompany/pooempregado/PooEmpregado.java
@@ -19,7 +19,7 @@ public class PooEmpregado {
 
         System.out.println("\nSalário mensal com aumento de 10%: ");
         System.out.println("Empregado 1: " + empregado1.getNome() + " " + empregado1.getSobrenome() + ", R$ " + empregado1.getSalarioMensal()); //  salarioMensal com aumento de 10% nó salarioMensal
-        System.out.println("Empregado 2: " + empregado2.getNome() + " " + empregado2.getSobrenome() + ", R$ " + empregado2.salarioMensal()); 
+        System.out.println("Empregado 2: " + empregado2.getNome() + " " + empregado2.getSobrenome() + ", R$ " + empregado2.getSalarioMensal()); 
         
         System.out.println("\nSalário anual com aumento de 10%: ");
         System.out.println("Empregado 1: " + empregado1.getNome() + " " + empregado1.getSobrenome() + ", R$ " + empregado1.salarioAnual()); // puxa funcão retorno salarioAnual (salarioMensal * 12) com aumento de 10% nó salarioMensal
