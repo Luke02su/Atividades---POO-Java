d0c02d0ea0d2288f49ba739d67dec1e7f432837b
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-06-23 21:31:12 -0300

    Update Biblioteca.java

--- testeBiblioteca/src/main/java/com/mycompany/testebiblioteca/Biblioteca.java
+++ testeBiblioteca/src/main/java/com/mycompany/testebiblioteca/Biblioteca.java
@@ -2,52 +2,78 @@ package com.mycompany.testebiblioteca;
 
 public class Biblioteca {
     
-    private Livro livros[];
-    private final int MAX_LIVROS = 10;
+    private Livro[] livros;
+    private int MAX_LIVROS;
     private int qtdLivros;
-            
+    
     public Biblioteca () {
-        livros = new Livro[MAX_LIVROS];
-        qtdLivros = 0;
+        this.setMAX_LIVROS(2);
+        this.setQtdLivros(0);
+        this.setLivros(new Livro[getMAX_LIVROS()]);
+    }
+    
+    public Livro[] getLivros() {
+        return this.livros;
+    }
+    
+    public void setLivros(Livro[] livros) {
+        this.livros = livros;
+    }
+    
+    public int getMAX_LIVROS() {
+        return this.MAX_LIVROS;
+    }
+    
+    public void setMAX_LIVROS(int MAX_LIVROS){
+        this.MAX_LIVROS = MAX_LIVROS;
+    }
+    
+    public int getQtdLivros() {
+        return this.qtdLivros;
+    }
+    
+    public void setQtdLivros(int qtdLivros) {
+        this.qtdLivros = qtdLivros;
     }
   
     public void lerr () {
-        if (qtdLivros >= MAX_LIVROS) {
+        if (this.getQtdLivros() >= this.getMAX_LIVROS()) {
             System.out.println("Biblioteca cheia.\n");
             return;
         }
-        livros[qtdLivros].ler();
-        qtdLivros++;
+        this.livros[this.getQtdLivros()] = new Livro();
+        this.getLivros()[this.getQtdLivros()].ler();
+        this.setQtdLivros(this.getQtdLivros() + 1);
     }
   
     public void imprimirr () {
       
-        if (qtdLivros == 0) {
+        if (this.getQtdLivros() == 0) {
             System.out.println("Biblioteca vazia.\n");
             return;
         }
       
         for (int i = 0; i < qtdLivros; i++) {
-            livros[i].imprimir();
+            this.getLivros()[i].imprimir();
         }
     }
   
     public void maiorPagina () {
       
-        int maiorNumPaginas = livros[0].numPaginas;
+        int maiorNumPaginas = this.getLivros()[0].getNumPaginas();
         int livro = 0;
          
-        if (qtdLivros == 0) {
+        if (this.getQtdLivros() == 0) {
               System.out.println("Biblioteca vazia.\n");
               return;
         }
          
         for (int i = 1; i < qtdLivros; i++) {
-            if (maiorNumPaginas < livros[i].numPaginas) {
-                maiorNumPaginas = livros[i].numPaginas;
+            if (maiorNumPaginas < this.getLivros()[i].getNumPaginas()) {
+                maiorNumPaginas = this.getLivros()[i].getNumPaginas();
                 livro = i;
             }
         }
-     livros[livro].imprimir();
+     this.getLivros()[livro].imprimir();
     }
 }
