470b66636c7161f5395087b9970f4ae52f0d83e7
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-06-27 07:51:13 -0300

    Update PecaImportada.java

--- Atividade04/src/main/java/com/mycompany/atividade04/PecaImportada.java
+++ Atividade04/src/main/java/com/mycompany/atividade04/PecaImportada.java
@@ -49,16 +49,16 @@ public class PecaImportada extends Produto { // subclasse peça é extensão da 
     
     //Métodos da própria classe
     public void imprimirPecaImportada() {
-        System.out.println("----- PECA IMPORTADA -----");
-        System.out.println("Código da peça: " + getCodigo());
-        System.out.println("Nome da peça: " + getNome());
-        System.out.println("Preço de  custo: R$" + getPrecoCusto());
-        System.out.println("Margem de lucro: " + getMargemLucro() + "%");
-        System.out.println("Preço de venda: R$" + getPrecoVenda());
-        System.out.println("Marca do veículo: " + getMarcaVeiculo());
-        System.out.println("Modelo do veículo: " + getModeloVeiculo());
-        System.out.println("Ano inicial de produção: " + getAnoInicial());
-        System.out.println("Ano final de produção: " + getAnoFinal());
+        System.out.println("----- PECA IMPORTADA -----"); // poderia reaproveitar a impressão de prodruto usando super.imprimir(), sem ter a necessidade de duplicar os comandos
+        System.out.println("Código da peça: " + super.getCodigo());
+        System.out.println("Nome da peça: " + super.getNome());
+        System.out.println("Preço de  custo: R$" + super.getPrecoCusto());
+        System.out.println("Margem de lucro: " + super.getMargemLucro() + "%");
+        System.out.println("Preço de venda: R$" + super.getPrecoVenda());
+        System.out.println("Marca do veículo: " + super.getMarcaVeiculo());
+        System.out.println("Modelo do veículo: " + super.getModeloVeiculo());
+        System.out.println("Ano inicial de produção: " + super.getAnoInicial());
+        System.out.println("Ano final de produção: " + super.getAnoFinal());
         System.out.println("Revendedor: " + this.getRevendedor());
         System.out.println("Valor do frete: " + this.getValorFrete());
         System.out.println("Valor do dólar: R$" + this.getValorDolar());
@@ -68,8 +68,8 @@ public class PecaImportada extends Produto { // subclasse peça é extensão da 
     }
     
     private double calcularValorImportacao() {
-        if((getPrecoCusto() / getValorDolar()) > 50.00) {
-            return this.getValorImportacao() + getPrecoCusto() + (getPrecoCusto() * (62.00 / 100.00)); // Considere que todo produto acima de 50 dólares é taxado em 62% sobre seu valor de custo. Utilize a contação do dólar atual para realização do cálculo
+        if((super.getPrecoCusto() / this.getValorDolar()) > 50.00) {
+            return this.getValorImportacao() + super.getPrecoCusto() + (super.getPrecoCusto() * (62.00 / 100.00)); // Considere que todo produto acima de 50 dólares é taxado em 62% sobre seu valor de custo. Utilize a contação do dólar atual para realização do cálculo
         } else {
             return this.getValorImportacao();
         }
@@ -84,13 +84,13 @@ public class PecaImportada extends Produto { // subclasse peça é extensão da 
     //Métodos abstratos implementados
     @Override
     public void calcularPrecoVenda() {
-        setPrecoVenda(getPrecoCusto() + (getPrecoVenda() * (getMargemLucro() / 100.00))); //O preço de venda é o preço de custo + % margem de lucro.
-        setPrecoVenda(getPrecoVenda() + getValorFrete() + calcularValorImportacao());  //Para peças importadas, junto ao preço de venda deve ser adicionado o valor do frete + valor importação
+        super.setPrecoVenda(super.getPrecoCusto() + (super.getPrecoVenda() * (super.getMargemLucro() / 100.00))); //O preço de venda é o preço de custo + % margem de lucro.
+        super.setPrecoVenda(super.getPrecoVenda() + this.getValorFrete() + calcularValorImportacao());  //Para peças importadas, junto ao preço de venda deve ser adicionado o valor do frete + valor importação
     }
     
     @Override
     public void imprimirValorDetalhado() {
-        System.out.println("Valor de venda da peça importada: R$" + this.getPrecoVenda()); 
+        System.out.println("Valor de venda da peça importada: R$" + super.getPrecoVenda()); 
         
         System.out.println();
     }
