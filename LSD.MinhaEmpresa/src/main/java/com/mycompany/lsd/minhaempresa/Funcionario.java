40b95b8af17b63a1849abf58615d41af6f480693
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-07-21 00:33:20 -0300

    Update Funcionario.java

--- LSD.MinhaEmpresa/src/main/java/com/mycompany/lsd/minhaempresa/Funcionario.java
+++ LSD.MinhaEmpresa/src/main/java/com/mycompany/lsd/minhaempresa/Funcionario.java
@@ -11,21 +11,13 @@ public class Funcionario extends Pessoa {
         
     }
 
-    public Funcionario( String nome, int numeroCPF, int numeroRegistro, double salario, double comissao) {
+    public Funcionario(String nome, int numeroCPF, int numeroRegistro, double salario, double comissao) {
         super(nome, numeroCPF);
         this.numeroRegistro = numeroRegistro;
         this.salario = salario;
         this.comissao = comissao;
         this.numeroTotalFuncionarios++;
         super.setPertenceObjeto(false);
-    }
- 
-    public static int getNumeroTotalFuncionarios() {
-        return numeroTotalFuncionarios;
-    }
-
-    public static void setNumeroTotalFuncionarios(int aNumeroTotalFuncionarios) {
-        numeroTotalFuncionarios = aNumeroTotalFuncionarios;
     }
 
     public int getNumeroRegistro() {
@@ -52,6 +44,14 @@ public class Funcionario extends Pessoa {
         this.comissao = comissao;
     }
     
+    public int getNumeroTotalFuncionarios() {
+        return numeroTotalFuncionarios;
+    }
+
+    private void setNumeroTotalFuncionarios(int aNumeroTotalFuncionarios) {
+        numeroTotalFuncionarios = aNumeroTotalFuncionarios;
+    }
+    
     @Override
     public void imprimir() {
         System.out.println("\n--- DADOS DE FUNCIONÁRIOS --- ");
@@ -63,13 +63,34 @@ public class Funcionario extends Pessoa {
     }
     
     public void aumentarSalario() {
-        System.out.println("--\n --- SALÁRIO DE FUNCIONÁRIO AUMENTADO EM 10% ---");
+        System.out.println("\n--- SALÁRIO DE FUNCIONÁRIO AUMENTADO VIA PREDEFINIÇÃO ---");
         this.setSalario(this.getSalario() * 1.10);
+        this.imprimirFuncionario("Aumento em 10%.");
     }
     
     public void aumentarSalario(int porcentagem) {
-        System.out.println("\n --- SALÁRIO DE FUNCIONÁRIO AUMENTADO VIA PARÂMETRO EM ---" + porcentagem);
-        this.setSalario(this.getSalario() + (this.getSalario() * ((double)porcentagem / 100.00)));
+        System.out.println("\n--- SALÁRIO DE FUNCIONÁRIO AUMENTADO VIA PARÂMETRO---");
+        if (porcentagem <= 50) {
+            this.setSalario(this.getSalario() + (this.getSalario() * ((double)porcentagem / 100.00)));
+            this.imprimirFuncionario("Aumento em " + porcentagem + "%.");
+        } else {
+            System.out.println("ERRO: Salário não pode ser aumentado em mais de 50% em uma vez.");
+        }
+    }
+    
+    private void imprimirFuncionario(String mensagem) {
+        System.out.println("\n--- DADOS DE FUNCIONÁRIOS ATUALIZADOS ---");
+        System.out.println("Nome: " + super.getNome());
+        System.out.println("Número de registro: " + this.getNumeroRegistro());
+        System.out.println("Salário: " + this.getSalario());
+        System.out.println("Mensagem: " + mensagem);
+    }
+    
+    public void imprimirSalarioComissao() {
+        System.out.println("\n--- SOMA DA COMISSÃO AO SALÁRIO NO FINAL DO MÊS ---");
+        this.setSalario(this.getSalario() + this.getComissao());
+        System.out.println("Nome do funcionário: " + super.getNome());
+        System.out.println("Valor em R$" + this.getSalario());
     }
   
 }
